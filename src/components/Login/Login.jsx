b873952b49cccbb2abbfabeb364401509b3d95c2
michelle vegas <michellevegas_16@hotmail.com>
2023-09-09 17:49:33 -0300

    formulario do login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,45 @@
+import {useState} from "react";
 function Login(){
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    
+    const handleEmailChange = (e) =>{
+        setEmail(e.target.value);
+    };
+    const handleSenhaChange = (e) =>{
+        setSenha(e.target.value)
+    }
+    const handleLogin = () =>{
+
+    }
     return(
-     <p>
-      Login   
-    </p>
+     <div className="">
+        <h2>Login</h2>
+        <form className="form-login">
+            <div className="email">
+                <label htmlFor="email">Email:</label>
+                <input 
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                />
+            </div>
+            <div className="password">
+                <label htmlFor="password">Senha:</label>
+                <input 
+                type="password"
+                id="password"
+                value={senha}
+                onChange={handleSenhaChange}/>
+            </div>
+            <button type="button" onClick={handleLogin} className="btn-login">
+               Entrar
+            </button>
+            <p>Não possui uma conta? <a>criar uma conta nova</a></p>
+            <a>Esqueci minha senha</a>
+        </form>
+     </div>
     )
 }
 
