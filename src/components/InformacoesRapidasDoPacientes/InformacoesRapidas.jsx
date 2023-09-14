cde5f2c0d19697873b9b14f5ba182fa5f088a1be
michelle vegas <michellevegas_16@hotmail.com>
2023-09-14 15:51:58 -0300

    glass no input

--- src/components/InformacoesRapidasDoPacientes/InformacoesRapidas.jsx
+++ src/components/InformacoesRapidasDoPacientes/InformacoesRapidas.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './InformacoesRapidas.css'
+
 
 function InformacoesRapidasDoPaciente() {
   const [search, setSearch] = useState("");
+ 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
   };
 
+  
   return ( 
 
     <div className="container-inf">
       <h1 className="title-search">Informações Rapidas Dos Pacientes</h1>
       <div className="container-search">
-        <input className="input-search" type="search" value={search} onChange={handleSearchChange} />
-        <button className="btn-search">
+        <input className="input-search" type="search" placeholder="Digite o nome do paciente" value={search} onChange={handleSearchChange} />
+        <span className="icon">
+          <FontAwesomeIcon icon={faSearch} />
+        </span>
+        <button className="btn-search" >
         Buscar
       </button>
       </div>
